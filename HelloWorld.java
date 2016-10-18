57524e686a232f6ece14c6e164a84a9c99f6de85
Vladislav  Unchuris <vlad.unchuris@gmail.com>
2016-10-18 22:03:11 +0600

    test

--- HelloWorld.java
+++ HelloWorld.java
@@ -4,10 +4,9 @@ class HelloWorld {
     public static void main(String[] args){
         Date date = new Date();
         System.out.println("Hello World");
-		
-		for(int i=0; i < args.length; i++)
-		System.out.println(args[i]);
-		
+		for(int i=0; i < args.length; i++){
+			System.out.println(args[i]);
+		}
         System.out.println("Date: " + date);
     }
 }
